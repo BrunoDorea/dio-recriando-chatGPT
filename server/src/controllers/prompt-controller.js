d1f5490bc7f5ea0378e871ee68e2008a2c969bab
Bruno Henrique <brunodorea@outlook.com.br>
2023-03-08 22:00:03 -0300

    corrigindo

--- server/src/controllers/prompt-controller.js
+++ server/src/controllers/prompt-controller.js
@@ -1,23 +1,27 @@
-const openai = require('../config/openai')
-const InputPrompt = require('../model/input-model')
+const inputPrompt = require("../models/input-prompt")
+const openai = require("../config/openai")
 
 module.exports = {
-  async sendText(request, response) {
-    const openaiAI = openai.configuration()
-    const inputModel = new InputPrompt(request.body)
+	async sendText(req, res){
 
-    try {
-      const response = await openaiAI.createCompletion(openai.textCompletion(inputModel))
+		const openaiAPI = openai.configuration()
+		const inputModel = new inputPrompt(req.body)
 
-      return response.status(200).json({
-        sucess: true,
-        data: response.data.choices[0].text
-      })
-    } catch (error) {
-      return response.status(400).json({
-        sucess: false,
-        error: error.response ? error.response.data : 'Error no servidor'
-      })
-    }
-  }
+		try {
+			const response = await openaiAPI.createCompletion(
+				openai.textCompletion(inputModel)
+			)
+
+			return res.status(200).json({
+				sucess: true,
+				data: response.data.choices[0].text
+			})
+		} catch (error) {
+
+			return res.status(400).json({
+				sucess: false,
+				error: error.response ? error.response.data : 'Error no servidor'
+			})
+		}
+	}
 }
